05625caecbe55d192f5d3d02152e3c3951cb6cb9
oreoA09 <oreo.akanni@gmail.com>
2022-09-24 19:14:18 +0000

    understanding importing & exporting

--- setup/src/App.js
+++ setup/src/App.js
@@ -1,8 +1,16 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+import React from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  return (
+<main>
+    <section class="container">
+  <h2 style={{color:"green"}}>reminder project setup</h2>;
+  <button>Delete All</button>
+  </section>
+  </main>
+  )
 }
 
 export default App;
